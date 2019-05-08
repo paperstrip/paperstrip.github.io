24c96ad8b63a761d2cd0617aa45883349b8ae774
Arnaud Herr <arnaudherr@MacBook-Pro-de-Arnaud.local>
2019-05-08 15:02:27 +0200

    favico + fix bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,11 @@ window.$ = jquery;
 
 import Barba from 'barba.js'; // Or nothing if loaded via the browser
 import * as THREE from 'three/build/three.min.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-
 import TweenMax from "gsap/TweenMax";
-import scrollify from "jquery-scrollify";
 import anime from 'animejs/lib/anime.es.js';
 import skrollr from 'skrollr';
 import slick from "slick-carousel";
-
+import luxy from "luxy.js";
 
 (function($) {
     function injector(t, splitter, klass, after) {
@@ -80,122 +77,17 @@ var mobileAndTabletcheck = function() {
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
+
 Barba.Pjax.start();
 
 
-/*===========================*/
-/*													 */
-/*													 */
-/*CANVAS*/
-/*													 */
-/*													 */
-/*===========================*/
-/*
-const canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d");
-var colorPallete = ["#FFA4BC", "#C5D3FB", "#97F9FF"];
-var rayon = 10;
 
-var width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight,
-    origin = {
-        x: window.innerWidth - 20,
-        y: window.innerHeight/2 - 20
-    },
-    mouse = {
-        x: window.innerWidth - 20,
-        y: window.innerHeight/2 - 20
-    },
-    balls = [],
-    count = 0,
-    randomCount = 1;
-
-window.onresize = () => {
-    width = canvas.width = window.innerWidth;
-    height = canvas.height = window.innerHeight;
-    origin = {
-        x: window.innerWidth - 20,
-        y: window.innerHeight/2 - 20
-    };
-}
-
-class Ball {
-    constructor() {
-        this.x = origin.x;
-        this.y = origin.y;
-        this.angle = Math.PI * 2 * Math.random();
-        this.vx = (1.3 + Math.random() * .2) * Math.cos(this.angle);
-        this.vy = (1.3 + Math.random() * .2) * Math.sin(this.angle);
-        this.r = Math.floor(rayon + 3 * Math.random());
-        this.color = colorPallete[Math.floor(Math.random() * colorPallete.length)];
-
-    }
-
-    update() {
-        this.x += this.vx / 2;
-        this.y += this.vy /2;
-        this.r -= .01;
-
-    }
-}
-
-
-function loop() {
-    context.clearRect(0, 0, width, height);
-    if (count === randomCount) {
-        balls.push(new Ball());
-        count = 0;
-        randomCount = 3 + Math.floor(Math.random() * 5);
-    }
-    count++;
-    for (var i = 0; i < balls.length; i++) {
-        var b = balls[i];
-        context.fillStyle = b.color;
-        context.beginPath();
-        context.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
-        context.fill();
-        b.update();
-    }
-
-    origin.x += (mouse.x - origin.x) * .15;
-    origin.y += (mouse.y - origin.y) * .15;
-
-    context.fillStyle = "white";
-    context.beginPath();
-    context.arc(origin.x, origin.y, 40, 0, Math.PI * 2, false);
-    context.fill();
-
-    removeBall();
-    requestAnimationFrame(loop);
-}
-
-function removeBall() {
-    for (var i = 0; i < balls.length; i++) {
-        var b = balls[i];
-        if (
-            b.x + b.r < 0 ||
-            b.x - b.r > width ||
-            b.y + b.r < 0 ||
-            b.y - b.r > height ||
-            b.r < 0
-        ) {
-            balls.splice(i, 1);
-        }
-    }
-}
-
-window.addEventListener("click", function(e) {
-    mouse.x = e.clientX;
-    mouse.y = e.clientY;
-
-}, false);*/
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
 function hasScrolled(elm) {
     var st = $(window).scrollTop();
-    console.log(st)
     // Make sure they scroll more than delta
     if(Math.abs(lastScrollTop - st) <= delta)
         return;
@@ -245,12 +137,15 @@ function onReady(callback) {
             window.clearInterval(intervalId);
             callback.call(this);
         }
-    }, 100);
+    }, 1000);
 }
 
 
 onReady(function() {
+  /*luxy.init({
+    wrapperSpeed:  0.1
 
+  });*/
   $(".letter-animation").lettering();
 
     var tl = new TimelineMax({
@@ -260,21 +155,6 @@ onReady(function() {
         onComplete: animationLetter
     });
 
-    var polylion_staggerFrom = {
-      scale: 0,
-      opacity: 0,
-      transformOrigin: 'center center',
-    };
-
-    var polylion_staggerTo = {
-      opacity: 1,
-      scale: 1,
-      ease: Elastic.easeInOut
-    };
-    var polylion_shapes   = $('polygon'),
-    polylion_stagger  = 0.00475,
-    polylion_duration = 1.5;
-    //tl.staggerFromTo(polylion_shapes, polylion_duration, polylion_staggerFrom, polylion_staggerTo, polylion_stagger, 0);
     tl.staggerFromTo("#loading-screen", 1, {
         autoAlpha: 1,
         ease: Power4.easeInOut
@@ -299,7 +179,6 @@ onReady(function() {
     }*/
     $(window).scroll(function(event){
         didScroll = true;
-        console.log('scroll')
     });
 
     setInterval(function() {
@@ -429,7 +308,6 @@ function initHomePage() {
 }
 
 function initStagger() {
-  console.log('stagge');
     var toAnimate = new TimelineMax({delay:.5});
     toAnimate.staggerFromTo(".toStagger", 0.5, {
         ease: Power4.easeInOut,
@@ -464,7 +342,6 @@ function annimationBlob() {
 }
 
 function animationLetter() {
-  console.log('AnimeLetter');
     var toAnimate = new TimelineMax();
     toAnimate.staggerFromTo(".letter-animation span", 0.5, {
         ease: Back.easeOut.config(1.7),
@@ -491,8 +368,8 @@ function initSlideShow(){
     slidesToScroll: 1,
     prevArrow: $('.prev-slick'),
     nextArrow: $('.next-slick'),
-
-    infinite: true,
+    dots:true,
+    infinite: false,
     waitForAnimate:true,
     arrows: true,
     onAfterChange: function(slide, index){
@@ -510,9 +387,6 @@ function initSlideShow(){
 
   });
   // On before slide change
-
-
-
   slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
 
     var morphingCase = anime({
@@ -534,65 +408,21 @@ function initSlideShow(){
       var coverUrlCurrent = $('.case-list__item').eq(currentSlide).data('cover');
       var coverUrlNext = $('.case-list__item').eq(nextSlide).data('cover');
       if (mobileAndTabletcheck() == true) {
-        $('.effect-background').css("background-image", "url('src/img/1500/"+coverUrlCurrent+"')");
+        $('.effect-background').css("background-image", "url('src/img/1500/"+coverUrlNext+"')");
       }
       else {
         var myAnimation = new hoverEffect({
           parent: document.querySelector('.effect-background'),
-          image1: 'src/img/1500/' + coverUrlNext,
-          image2: 'src/img/1500/' + coverUrlCurrent,
+          image1: 'src/img/1500/' + coverUrlCurrent,
+          image2: 'src/img/1500/' + coverUrlNext,
           displacementImage: 'src/img/1500/distorsion.jpg'
         });
       }
 
-      //
   });
-//scrollSlideshow()
-function scrollSlideshow(){
-  $('.vertical-slider').one('mousewheel', function(e) {
-    var delta = e.originalEvent.wheelDelta;
-    e.preventDefault();
-    if (delta < 0) {
-
-
-      $('.vertical-slider').slick('slickNext');
-      $('.vertical-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-
-        if (currentSlide == 0 ){
-
-          $.scrollify.instantNext();
-        }
-        else{
-        }
-
-      })
-
-    } else {
-      $('.vertical-slider').slick('slickPrev');
-      $('.vertical-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-
-        if (currentSlide == 0 ){
-        //  $.scrollify.enable();
-        }
-        else{
-        //  $.scrollify.disable();
-
-        }
-
-      })
-    }
-
-  });
-
-
-}
-slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
-  //scrollSlideshow();
-});
 
 }
 
-//initSlideShow();
 
 
 
@@ -726,7 +556,6 @@ var Homepage = Barba.BaseView.extend({
     },
     onLeave: function() {
         // A new Transition toward a new page has just started.
-        console.log('leaving');
       //  $.scrollify.instantPrevious();
 
     },
@@ -775,7 +604,6 @@ var Works = Barba.BaseView.extend({
     },
     onLeave: function() {
         // A new Transition toward a new page has just started.
-        console.log('leaving');
       //  $.scrollify.instantPrevious();
 
     },
@@ -991,7 +819,7 @@ function nextSlide(){
       }
 
 			function initWebGl() {
-				container = document.getElementsByTagName( 'body' );
+				container = document.getElementsByClassName( 'home-container' );
 				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
 				camera.position.z = 3200;
 				scene = new THREE.Scene();
